fc612e43c78b9d6f837f03e68d65b2661cb7a02c
Denis R <rafael@xystudio.ru>
2021-08-26 21:52:34 +0300

    add top bar

--- template/inc/footer/3columns.php
+++ template/inc/footer/3columns.php
@@ -2,11 +2,13 @@
 <div class="footer-grid-3 footer-grid-holder">
 	<div class="footer-widget-1">
 		<?php if ( is_active_sidebar( 'fsidebar-1' ) ) : dynamic_sidebar( 'fsidebar-1' ); endif; ?>
+
 	</div>
 	<div class="footer-widget-2">
 		<?php if ( is_active_sidebar( 'fsidebar-2' ) ) : dynamic_sidebar( 'fsidebar-2' ); endif; ?>	
+		<?php if ( is_active_sidebar( 'fsidebar-3' ) ) : dynamic_sidebar( 'fsidebar-3' ); endif; ?>
 	</div>
 	<div class="footer-widget-3">
-		<?php if ( is_active_sidebar( 'fsidebar-3' ) ) : dynamic_sidebar( 'fsidebar-3' ); endif; ?>
+		<?php if ( is_active_sidebar( 'fsidebar-4' ) ) : dynamic_sidebar( 'fsidebar-4' ); endif; ?>
 	</div>
 </div>
