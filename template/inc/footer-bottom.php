6f94e6648d65d3b879d3fe9baab3240fe3a173a0
Denis R <rafael@xystudio.ru>
2021-08-27 11:44:22 +0300

    change bottom bar

--- template/inc/footer-bottom.php
+++ template/inc/footer-bottom.php
@@ -2,54 +2,14 @@
 
 	<div class="fbottom-left">
 		
-		<?php if ( 'true' === get_theme_mod( 'fbottom_content_option', 'false' ) ) {
-		
-			echo mediaconsult_social_media_profiles();
-		
-		} else { ?>
-		
-		<nav>
-
-			<?php wp_nav_menu(
-				array(
-					'menu' 					=> 'mediaconsult-footer-menu',
-					'theme_location' 		=> 'mediaconsult-footer-menu',
-					'menu_class' 			=> 'footer-menu-list',
-					'depth'					=> 1,
-					'fallback_cb' 			=> false
-				)
-
-
-			); ?>
-
-		</nav>	
-		
-		<?php } ?>		
+		<a href="//xystuido.ru" target="_blank">Разработано в XYstudio</a>
 
 	</div>
 
 	<div class="fbottom-right">
 
-		<?php if( get_theme_mod( 'footer_copyright_text' ) ) { ?>
-
-			<div class="impressum">
-
-				<?php echo wp_kses( get_theme_mod( 'footer_copyright_text' ),
-					array(
-						'a' => array(
-							'href' => array(),
-							'title' => array()
-						),
-						'strong' => array(),
-						'em' => array(),
-						'br' => array(),
-						'span' => array(),
-					)
-				); ?>							
-
-			</div>
-
-		<?php } ?>
+		&copy; Все права защищены | ГКУ “Центр документации” <?php echo date('Y'); ?><br/>
+		Использование материалов сайта без согласования с ГКУ “Центр документации” запрещено. 	
 
 	</div>
 
